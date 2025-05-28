afba5c865d7b88d5877a360888b3b503976287f6
b1400 <benjamin@brown-fr.com>
2025-05-28 11:26:59 +0200

    ok2

--- index.php
+++ index.php
@@ -1,8 +1,6 @@
 <?php
 
-/* 
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHP.php to edit this template
- */
+header('Location: app/router/router2.php?action=caveAccueil');
 
 ?>
+
