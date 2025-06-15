738aba3852793f1f39fe417e04a8e60f32b95623
b1400 <benjamin@brown-fr.com>
2025-06-15 19:07:52 +0200

    la partie etu marche pas encore

--- app/model/ModelRdv.php
+++ app/model/ModelRdv.php
@@ -1,15 +1,18 @@
 <?php
 
 require_once 'Model.php';
+require_once 'ModelCreneau.php';
 
 class ModelRdv {
-    private $id,$creneau,$etudiant;
-    
+
+    private $id, $creneau, $etudiant;
+
     public function __construct($id, $creneau, $etudiant) {
         $this->id = $id;
         $this->creneau = $creneau;
         $this->etudiant = $etudiant;
     }
+
     public function getId() {
         return $this->id;
     }
@@ -34,6 +37,30 @@ class ModelRdv {
         $this->etudiant = $etudiant;
     }
 
+    public static function getRdvEtu($etu_id) {
+        try {
 
+            $database = Model::getInstance();
+            $query = "SELECT creneau FROM rdv WHERE etudiant = $etu_id";
+            $statement = $database->prepare($query);
+            $statement->execute();
+            $creneau_id = $statement->fetchAll(PDO::FETCH_COLUMN, 0);
+            if (empty($creneau_id)) {
+                return []; // No creneau found, return empty array
+            }
 
+            $placeholders = implode(',', array_fill(0, count($creneau_id), '?'));
+
+            $query = "SELECT projet, examinateur, creneau FROM creneau WHERE id IN ($placeholders)";
+            $statement = $database->prepare($query);
+            $statement->execute($creneau_id);
+
+            $results = $statement->fetchAll(PDO::FETCH_CLASS, "ModelCreneau");
+
+            return $creneau_id;
+        } catch (PDOException $e) {
+            printf("%s - %s<p/>\n", $e->getCode(), $e->getMessage());
+            return NULL;
+        }
+    }
 }
